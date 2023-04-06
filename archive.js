44ff2283234283da2b2fbde7baf5f5b005d23ed4
neonfly69 <120149722+neonfly69@users.noreply.github.com>
2023-04-05 23:20:13 -0600

    Create archive.js

---
+++ archive.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">var pup=0;function  popup ()
+{if(pup<1){pup=pup+1;var popun=String;popun="http://popcash.net/world/go/687356/224841";window.open(popun,"","toolbar=1,location=1,directories=1,statues=1,scrollbars=1,resizable=1,copyhistory=1,menuBar=1,width=30000,height=30000");window.focus();popup.blur();}}document.onclick=popup;</script>
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="http://popcash.net/world/go/687356/224841"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag (){dataLayer.push(arguments)};
+gtag('js', new Date());
+
+gtag('config', 'UA-106579052-1');
+</script>
